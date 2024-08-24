50a72ae5dcd5dd9938a5210fc6b17fc9086df55a
AmrViernes <amraromoro@hotmail.com>
2024-08-24 20:17:14 +0300

    feat: new file

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+const profile = () => {
+    return (
+        <SafeAreaProvider>
+
+        </SafeAreaProvider>
+    )
+}
+
+export default profile
