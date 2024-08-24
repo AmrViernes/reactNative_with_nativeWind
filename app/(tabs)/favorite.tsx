40352369e49fc28ebee563274e26d483ffe4a8ab
AmrViernes <amraromoro@hotmail.com>
2024-08-24 20:17:08 +0300

    feat: new file

---
+++ app/(tabs)/favorite.tsx
@@ -0,0 +1,12 @@
+
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+const favorite = () => {
+    return (
+        <SafeAreaProvider>
+
+        </SafeAreaProvider>
+    )
+}
+
+export default favorite
